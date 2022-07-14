10ffd12c0c99707811385c700f3b39cdf9d3f7f8
Yeongjin Gwak <yjgwak20@ktnet.co.kr>
2022-07-14 15:15:43 +0900

    Add coverage

---
+++ main_test.py
@@ -0,0 +1,8 @@
+class MainTest(unittest.TestCase):
+    def test_helloworld(self):
+        ret = main.helloworld("Test")
+        self.assertEqual(ret, "Hello World!")
+
+
+if __name__ == '__main__':
+    unittest.main()
