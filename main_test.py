122c35eb61a59e0890290189fb9ad1b5dffcfb77
Yeongjin Gwak <yjgwak20@ktnet.co.kr>
2022-07-14 15:29:48 +0900

    Fix mistake on function name

--- main_test.py
+++ main_test.py
@@ -5,7 +5,7 @@ import main
 
 class MainTest(unittest.TestCase):
     def test_helloworld(self):
-        ret = main.helloworld("Test")
+        ret = main.hello_world("Test")
         self.assertEqual(ret, "Hello World! Test")
 
 
